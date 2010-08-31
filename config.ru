71fe43ff6a9e4073e55a50a073b2d669a3d52fab
Glenn Goodrich <glenn.goodrich@gmail.com>
2010-08-31 16:59:02 -0400

    make bucket name part of the config

--- config.ru
+++ config.ru
@@ -9,4 +9,4 @@ require './legend_resource'
 run LegendResource
 # Uncomment this line to use Google Storage (don't forget to change the gstore.yml file)')
 #LegendResource.set :filehandler, GStoreLegend
-LegendResource.set :filehandler, S3Legend
+#LegendResource.set :filehandler, S3Legend
--- lib/backends/gstore_legend.rb
+++ lib/backends/gstore_legend.rb
@@ -9,42 +9,28 @@ class GStoreLegend
       :access_key => CONFIG['access_key'],
       :secret_key => CONFIG['secret']
     )
-    
+    @@bucket = CONFIG['bucket']
   def self.exists?( object_name)
-     
-      bucket_name, service_name = object_name.split(/_/,2)
       # So, Google Storage wants domain verification on buckets with dots in them
-      bucket_name.gsub!(".","_")
-      @@client.get_object(bucket_name, service_name).match(/does not exist/).nil?
-
+      object_name.gsub!(".","_")
+      @@client.get_object(@@bucket, object_name).match(/does not exist/).nil?
   end
   
   def self.delete(object_name)
-    bucket_name, service_name = object_name.split(/_/,2)
-    # So, Google Storage wants domain verification on buckets with dots in them
-    bucket_name.gsub!(".","_")
-    @@client.delete_object(bucket_name,service_name)
+    object_name.gsub!(".","_")
+    @@client.delete_object(@@bucket,object_name)
   end
   
   def self.write(data, object_name)
-    bucket_name, service_name = object_name.split(/_/,2)
     # So, Google Storage wants domain verification on buckets with dots in them
-    bucket_name.gsub!(".","_")
-    bucket_exists = @@client.get_bucket(bucket_name).match(/does not exist/).nil?
-    if (!bucket_exists)
-      puts @@client.create_bucket(bucket_name)
-    end
-    data.format='PNG'
-   
-    @@client.put_object(bucket_name, service_name, :data=>data.to_blob,:headers=>{:content_length=>data.to_blob.length,:content_type=>"image/png"})
+    @@bucket.gsub!(".","_")
+    @@client.put_object(@@bucket, object_name, :data=>data.to_blob,:headers=>{:content_length=>data.to_blob.length,:content_type=>"image/png"})
   end
   
   def self.send(object_name)
-    bucket_name, service_name = object_name.split(/_/,2)
     # So, Google Storage wants domain verification on buckets with dots in them
-    bucket_name.gsub!(".","_")
-
-    @@client.get_object(bucket_name, service_name)
+    @@bucket.gsub!(".","_")
+    @@client.get_object(@@bucket, object_name)
   end
   
 end
